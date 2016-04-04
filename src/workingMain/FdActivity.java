307a2b2f0a120587b36e17aab6ee49a940e321a7
docProj <g00283033@gmit.ie>
2016-04-04 23:28:23 +0100

    Added files via upload
    SQLite DB code included
    
    **bug when pressing new button for the second time - app closes**

--- src/workingMain/FdActivity.java
+++ src/workingMain/FdActivity.java
@@ -19,6 +19,7 @@ import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.imgproc.Imgproc;
 
+import android.widget.Toast; 
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -28,36 +29,57 @@ import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Button;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.Cursor;
+
 
 public class FdActivity extends Activity implements CvCameraViewListener2 {
 
     private static final String    TAG                 = "Project::MainActivity";
+    private static final String    DCDEBUG             = "darrynDebug";
     private static final Scalar    FACE_RECT_COLOR     = new Scalar(255, 0, 0, 255);	// Red
-    private static final Scalar    LINE_COLOR     	   = new Scalar(0, 0, 255);	// Green
+    private static final Scalar    LINE_COLOR     	   = new Scalar(0, 0, 255, 255);	// Green
+
+    private TextView numberOfRepsText;
+    private TextView lastDbRepEntry;
+    private Button repsToDB;
+    private SQLiteDatabase db;
 
     private Mat                    mRgba;
     private Mat                    mGray;
     private File                   mCascadeFile;
     private DetectionBasedTracker  mNativeDetector;
+    private float                  mRelativeFaceSize   = 0.2f;
+    private int                    mAbsoluteFaceSize   = 0;
+    private CameraBridgeViewBase   mOpenCvCameraView;
+
     private Point				   p1;
     private Point				   p2;
     private int screenHeight;
     private int screenWidth;
     
-    private float                  mRelativeFaceSize   = 0.2f;
-    private int                    mAbsoluteFaceSize   = 0;
-    private CameraBridgeViewBase   mOpenCvCameraView;
-    
     final Handler myHandler = new Handler();
     private int repCount = 0;
-    private TextView numberOfRepsText;
     int repTestFlag = 0;
+    
+    String id;
+    String repDisplay;
 
     final Runnable updateRepCountResult = new Runnable() {
     	public void run() {
     		updateRepCount();
+    	}
+    };
+    
+    final Runnable updateLastRepQuery = new Runnable() {
+    	public void run() {
+    		updateLastSet();
     	}
     };
 
@@ -124,27 +146,36 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
         mOpenCvCameraView.enableFpsMeter();
         
         numberOfRepsText =(TextView) findViewById(R.id.numberOfReps);
+        lastDbRepEntry =(TextView) findViewById(R.id.lastDbRepEntry);
+        repsToDB = (Button) findViewById(R.id.repsToDB);
         
-        // Get screen width and height
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        screenHeight = metrics.heightPixels;
-        screenWidth = metrics.widthPixels;
-        p1 = new Point(0,screenHeight-600);
-        p2 = new Point(screenWidth,screenHeight-600);
+        repsToDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    insertDbData();
+                    readDbData();
+                    repCount = 0;
+
+                } catch (Exception e) {
+                    Log.i(DCDEBUG, "ERROR WITH ONLICK LISTENER: " + e.getMessage());
+                }
+            }
+        });    
+
+        getScreenHeightWidth();
+        openDatabase();
     }
 
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         super.onPause();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
 
     @Override
-    public void onResume()
-    {
+    public void onResume() {
         super.onResume();
         if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
@@ -155,9 +186,109 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
         }
     }
 
+    public void openDatabase() {
+        try {
+            String SDcardPath = "data/data/org.opencv.samples.facedetect";
+            String DBpath = SDcardPath + "/" + "projectDB.db";
+            Log.i(DCDEBUG, "DB Path: " + DBpath);
+            db = SQLiteDatabase.openDatabase(DBpath, null, SQLiteDatabase.CREATE_IF_NECESSARY);
+            Log.i(DCDEBUG, "DB Opened ");
+        } catch (SQLiteException e) {
+            Log.i(DCDEBUG, "Error opening DB: " + e.getMessage());
+            finish();
+        }
+        
+    }
+
+    public void insertDbData() {
+        // create the table
+        db.beginTransaction();
+        try {
+            // need to edit XML
+            db.execSQL("create table repTable("
+                + "tblID integer PRIMARY KEY autoincrement, "
+                + "Reps integer); ");
+            db.setTransactionSuccessful();
+            Log.i(DCDEBUG, "Table created successfully");
+        } catch (SQLException e1) {
+            Log.i(DCDEBUG, "Error creating table: " + e1.getMessage());
+            finish();
+        }
+
+        finally {
+            db.endTransaction();
+        }
+
+        // populate the table
+        db.beginTransaction();
+        try {
+            // need to edit XML
+            db.execSQL("insert into repTable(Reps) values ('"+repCount+"');");
+            db.setTransactionSuccessful();
+            Log.i(DCDEBUG, repCount + " inserted into table successfully");
+        } catch (SQLException e2) {
+            Log.i(DCDEBUG, "Error inserting into DB: " + e2.getMessage());
+            finish();
+        }
+
+        finally {
+            db.endTransaction();
+        }
+    }
+
+    public void readDbData() {
+    	int i1 = 0;
+    	int i2 = 1;
+        db.beginTransaction();
+
+        try {
+            Cursor curs =  db.rawQuery("SELECT * FROM repTable where tblID = (select max(tblID) from repTable)", null);
+            curs.moveToNext();
+
+            id = curs.getString(i1);
+            repDisplay = curs.getString(i2);
+
+            myHandler.post(updateLastRepQuery);
+            i1+=2; i2+=2;
+            db.setTransactionSuccessful();
+        } catch (SQLException e) {
+            Log.i(DCDEBUG, "Error reading from DB: " + e.getMessage());
+        }
+        finally {
+            db.endTransaction();
+        }
+    }
+
+    public void dropTable() {
+        // (clean start) action query to drop table
+    	db.beginTransaction();
+        try {
+            db.execSQL("DROP TABLE IF EXISTS repTable;");
+            db.setTransactionSuccessful();
+            Log.i(DCDEBUG, "Table dropped successfully");
+        } catch (Exception e) {
+            Log.i(DCDEBUG, "Table dropped error: " + e.getMessage());
+            finish(); 
+        }
+        finally {
+            db.endTransaction();
+        }
+    }
+
+    public void getScreenHeightWidth() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        screenHeight = metrics.heightPixels;
+        screenWidth = metrics.widthPixels;
+        p1 = new Point(0,screenHeight-600);
+        p2 = new Point(screenWidth,screenHeight-600);
+    }
+
     public void onDestroy() {
         super.onDestroy();
         mOpenCvCameraView.disableView();
+        dropTable();
+        db.close();
     }
 
     public void onCameraViewStarted(int width, int height) {
@@ -172,7 +303,6 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
     }
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-
         mRgba = inputFrame.rgba();
         mGray = inputFrame.gray();
 
@@ -208,7 +338,11 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
         return mRgba;
     }
     
-    public void updateRepCount(){
+    public void updateRepCount() {
     	numberOfRepsText.setText("Reps = " + String.valueOf(repCount));
+    }
+    
+    public void updateLastSet() {
+    	lastDbRepEntry.setText("Program Finished - Set: " + id + " Reps: " + repDisplay);
     }
 }
