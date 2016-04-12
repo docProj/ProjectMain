be64c83334f7c519cd165933bfc8256f8b74b5bc
docProj <g00283033@gmit.ie>
2016-04-12 17:06:30 +0100

    Added files via upload

---
+++ src/workingMain/FinalActivity.java
@@ -0,0 +1,17 @@
+package org.opencv.samples.facedetect;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FinalActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.finishpage);
+        
+    }
+    
+    public void readFromDB(){
+    	
+    }
+}
